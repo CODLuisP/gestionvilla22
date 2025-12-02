53df28ae22274b8ed11c7e5334795d61f05a1682
CODLuisP <esluiscabrera20@gmail.com>
2025-12-02 08:49:09 -0500

    new change 22

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+
+
 // Middleware vacío
 export function middleware(request: NextRequest) { // eslint-disable-line @typescript-eslint/no-unused-vars
   return NextResponse.next();
