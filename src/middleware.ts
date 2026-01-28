7d703c7b771805e64a3a3e6352332776ff7c1121
CODLuisP <esluiscabrera20@gmail.com>
2026-01-27 23:16:52 -0500

    Ultimos cambios de gestion villa

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import type { NextRequest } from "next/server";
 
 
 
-
-
 // Middleware vacío
 export function middleware(request: NextRequest) { // eslint-disable-line @typescript-eslint/no-unused-vars
   return NextResponse.next();
